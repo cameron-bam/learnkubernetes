e5d13704adce43d5b7aa916bba6352c1339b379b
Cameron White <cameron.white@ringcentral.com>
2020-08-08 17:16:44 -0600

    fixing bugs in terraform code

--- project/allenv/main.tf
+++ project/allenv/main.tf
@@ -1,19 +1,19 @@
 provider "google" {
     credentials = "${file(var.credentials)}" 
     project = "${var.project}"
-    region = "${var.regions[denver]}"
+    region = "${var.region}"
 }
 
 resource "google_container_cluster" "primary" {
     name = "${var.cluster_name}"
     network = "${var.network}"
-    location = "${var.regions[denver]}"
+    location = "${var.region}"
     initial_node_count = "${var.initial_node_count}"
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-    name = ${var.node_name}
-    location = "${var.regions[denver]}"
+    name = "${var.node_name}"
+    location = "${var.region}"
     cluster = google_container_cluster.primary.name
     node_count = "${var.node_count}"
     node_config {
