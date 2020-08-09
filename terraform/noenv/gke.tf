1f4dc55adb9803e575a8fb4c8b6451cdca25b94b
Cameron White <cameron.white@ringcentral.com>
2020-08-09 09:57:26 -0600

    removing old terraform code

--- terraform/noenv/gke.tf
+++
@@ -1,6 +0,0 @@
-resource "google_container_cluster" "primary" { 
-    name = "mycluster"
-    network = "default"
-    location = "us-west2-a"
-    initial_node_count = 1 
-}
--- terraform/noenv/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-    credentials = "${file("./auth/mykubernetesproject-285821-f95be9f750e5.json")}" 
-    project = "mykubernetesproject-285821"
-    region = "us-west-2a"
-}
